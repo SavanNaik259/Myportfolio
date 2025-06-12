5ddfc34f1542bd6bb4f130695d869c5b2d883f5c
Savan <githost.2007@gmail.com>
2025-06-13 00:34:32 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -9,11 +9,11 @@ const About =()=>{
       <h1 className="common-text">about us</h1>
       <hr className='w-25 mx-auto'/>
       <div className="card mb-3 r-card" style={{ maxWidth: '94vw',border:'none' }}>
-  <div className="row ">
+  /*<div className="row ">
     <div className="col-md-4 d-flex justify-content-center align-items-center">
      <LazyLoadImage effect="blur" src={aimg1} className=" a-img img-fluid text-center lazy-load-image" alt="..."/>
-    </div>
-    <div className="col-md-8 d-flex justify-content-center align-items-center">
+    </div>*/
+    <div className="col-md-12 d-flex justify-content-center align-items-center">
       <div className="card-body">
         <p className="card-text lh-base">•Proficient in HTML, CSS, JavaScript, React.js, Node.js, Express.js, and MongoDB.
 
