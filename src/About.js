c4bee88f5a51514911e25c10304357c5cab82e7f
Savan <githost.2007@gmail.com>
2025-06-13 07:45:09 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import aimg1 from './Images/IMG_20240207_190532.jpg';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 const About =()=>{
@@ -8,8 +9,11 @@ const About =()=>{
       <h1 className="common-text">about us</h1>
       <hr className='w-25 mx-auto'/>
       <div className="card mb-3 r-card" style={{ maxWidth: '94vw',border:'none' }}>
-  
-    <div className="col-md-12 d-flex justify-content-center align-items-center">
+  <div className="row ">
+    <div className="col-md-4 d-flex justify-content-center align-items-center">
+     <LazyLoadImage effect="blur" src={aimg1} className=" a-img img-fluid text-center lazy-load-image" alt="..."/>
+    </div>
+    <div className="col-md-8 d-flex justify-content-center align-items-center">
       <div className="card-body">
         <p className="card-text lh-base">•Proficient in HTML, CSS, JavaScript, React.js, Node.js, Express.js, and MongoDB.
 
